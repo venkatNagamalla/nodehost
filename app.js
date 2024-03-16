31718dd723035e442cf5569c826cd6030e2b6120
venkat <shivavenkat006@gmail.com>
2024-03-16 11:12:05 +0530

    node hosting1

---
+++ app.js
@@ -0,0 +1,30 @@
+
+const express = require("express")
+const app = express()
+const {open}  = require("sqlite")
+const sqlite3 = require("sqlite3")
+const path = require("path")
+const dbPath = path.join(__dirname,"app.db")
+let db = null
+
+const initializeDbAndServer = async() => {
+    try{
+        db = await open({
+            filename: dbPath,
+            driver: sqlite3.Database
+        })
+        app.listen(3004,()=>{
+            console.log("app starting")
+        })
+    }
+    catch(e){
+        console.log(`server error: ${e.message}`)
+        process.exit(1)
+    }
+}
+
+initializeDbAndServer()
+
+app.get("/", async(request, response)=>{
+    response.send("Hello ")
+})
